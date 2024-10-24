250c9fc6c4f3e882a59560670eb01d0f125cc9ab
Bori888 <ilyes.bori8@gmail.com>
2024-10-24 14:14:00 +0200

    kesz

---
+++ src/main/ValtozokMegint.java
@@ -0,0 +1,18 @@
+package main;
+
+public class ValtozokMegint {
+
+    public static void main(String[] args) {
+        String nev = "Bori";
+        System.out.println(nev);
+
+        nev = "Borbála";
+        System.out.println(nev +" pontjai: " + (12 +33));
+        System.out.println(12 +33 +" "+ nev + "pontjai");
+        
+        int osszPont = 12 +33+ 21;
+        System.out.println(nev +" összpontja: "+osszPont);
+
+    }
+
+}
